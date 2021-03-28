35828f72ddc94b446d4bf239c2e31f61ecf571f5
Oussama Moulana <cornos@gmail.com>
2021-03-28 12:25:29 +0200

    testing component in vue3

---
+++ src/components/PageHome.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Salam Alaikoum</div>
+  <div v-for="thread in threads" :key="thread.id">
+      <h2>{{thread.title}}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+          <p>{{users.find(u => u.id === posts.find(p => p.id === postId).userId).name}}</p>
+          <p>{{posts.find(p => p.id === postId).text}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  data () {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
