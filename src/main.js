6ce1f10d8ba50b70ee5753a5ff86cf9529358bfe
Oussama Moulana <cornos@gmail.com>
2021-03-28 17:59:56 +0200

    Tips for router configuration

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import PageHome from '@/components/PageHome'
-import * as VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-  { path: '/', component: PageHome }
+  {
+    path: '/',
+    name: 'Home',
+    component: PageHome
+  }
 ]
 
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHistory(),
+const router = createRouter({
+  history: createWebHistory(),
   routes
 })
 
