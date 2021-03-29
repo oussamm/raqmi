bbcb2bb36a59fc11d82728ab9aabd5d6248a7b7b
Oussama Moulana <cornos@gmail.com>
2021-03-29 02:12:05 +0200

    Add not found page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="col-full">
+        <h1>Not Found</h1>
+        <router-link :to="{name: 'Home'}">Read some coold threads</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
